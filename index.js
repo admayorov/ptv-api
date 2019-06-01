1a57e1653e49231ff497394b50ffc70c145e345c
IsabelSprengers <Izsy01@live.com.au>
2019-06-01 17:21:14 +1000

    Import key and devID from exports

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const HMAC = require('crypto-js/hmac-sha1');
+const vars = require('./local_vars');
 
 const route_type = 0;
 const stop_id = 1154;
 
-const requestText = `/v3/departures/route_type/${route_type}/stop/${stop_id}?devId=${devId}`;
-const signature = HMAC(requestText, key);
+const requestText = `/v3/departures/route_type/${route_type}/stop/${stop_id}?devId=${vars.devId}`;
+const signature = HMAC(requestText, vars.key);
 
 console.log(`requestText: ${requestText}`);
 console.log(`signature: ${signature}`);
