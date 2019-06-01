bf0af6d3acdd5f7c9ad50624dad2637bee118612
IsabelSprengers <Izsy01@live.com.au>
2019-06-01 17:28:21 +1000

    Moved signature and main to functions

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const HMAC = require('crypto-js/hmac-sha1');
 const vars = require('./local_vars');
 
-const route_type = 0;
-const stop_id = 1154;
 
-const requestText = `/v3/departures/route_type/${route_type}/stop/${stop_id}?devId=${vars.devId}`;
-const signature = HMAC(requestText, vars.key);
+function generateSignature(request) {
+    return HMAC(request, vars.key);
+}
 
-console.log(`requestText: ${requestText}`);
-console.log(`signature: ${signature}`);
+function main() {
+    const route_type = 0;
+    const stop_id = 1154;
+
+    const requestText = `/v3/departures/route_type/${route_type}/stop/${stop_id}?devId=${vars.devId}`;
+
+    signature = generateSignature(requestText);
+    console.log(`request: ${requestText}&signature=${signature}`);
+}
+
+main();
+
+
+
+
+
+
+
 
 
 
