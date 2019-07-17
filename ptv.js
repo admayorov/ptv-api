827c5dc350e64e6ffadea99b2d965c5113e05b02
Andrey Mayorov <a.d.mayorov@gmail.com>
2019-07-17 22:25:05 +1000

    Convert to class

--- ptv.js
+++ ptv.js
@@ -1,125 +1,130 @@
 "use strict";
 
 const HMAC = require('crypto-js/hmac-sha1');
-const vars = require('./local_vars');
+const local_vars = require('./local_vars');
 const axios = require('axios');
 const util = require('util');
 
-
-function generateSignature(request) {
-    return HMAC(request, vars.key);
-}
-
-async function makePTVrequest(baseString, params = {}) {
-    // Remove any trailing "/"
-    if (baseString[baseString.length - 1] == '/') {
-        baseString = baseString.slice(0, -1)
+class PTV {
+    constructor(vars) {
+        this.vars = vars;
     }
 
-    baseString += "?devId=" + vars.devId;
-    for (const [key, value] of Object.entries(params)) {
-        // Some params (e.g. platform_numbers) take arrays as a value and each element 
-        // must be inserted as a duplicate query parameter:
-        if (Array.isArray(value)) {
-            for (let entry of value) {
-                baseString += "&" + key + "=" + entry;
+    generateSignature(request) {
+        return HMAC(request, this.vars.key);
+    }
+
+    async makePTVrequest(baseString, params = {}) {
+        // Remove any trailing "/"
+        if (baseString[baseString.length - 1] == '/') {
+            baseString = baseString.slice(0, -1)
+        }
+
+        baseString += "?devId=" + this.vars.devId;
+        for (const [key, value] of Object.entries(params)) {
+            // Some params (e.g. platform_numbers) take arrays as a value and each element 
+            // must be inserted as a duplicate query parameter:
+            if (Array.isArray(value)) {
+                for (let entry of value) {
+                    baseString += "&" + key + "=" + entry;
+                }
+            }
+            else {
+                baseString += "&" + key + "=" + value;
             }
         }
-        else {
-            baseString += "&" + key + "=" + value;
+
+        baseString += "&signature=" + this.generateSignature(baseString);
+
+        console.debug(this.vars.baseURL + baseString)
+        let response;
+        try {
+            response = await axios.get(this.vars.baseURL + baseString);
+            return response.data;
         }
+        catch (e) {
+            console.error("There was an error making the PTV API request:" + e);
+            return {};
+        }
+
     }
 
-    baseString += "&signature=" + generateSignature(baseString);
+    async getDepartures(routeType, stopId, routeId, params = {}) {
+        let baseString = "/v3/departures";
 
-    console.debug(vars.baseURL + baseString)
-    let response;
-    try {
-        response = await axios.get(vars.baseURL + baseString);
-        return response.data;
-    }
-    catch (e) {
-        console.error("There was an error making the PTV API request:" + e);
-        return {};
-    }
+        // URI parameters:
+        baseString += "/route_type/" + routeType;
+        baseString += "/stop/" + stopId;
+        if (routeId) {
+            baseString += "/route/" + routeId;
+        }
 
-}
-
-async function getDepartures(routeType, stopId, routeId, params = {}) {
-    let baseString = "/v3/departures";
-
-    // URI parameters:
-    baseString += "/route_type/" + routeType;
-    baseString += "/stop/" + stopId;
-    if (routeId) {
-        baseString += "/route/" + routeId;
+        const result = await this.makePTVrequest(baseString, params);
+        return result;
     }
 
-    const result = await makePTVrequest(baseString, params);
-    return result;
-}
-
-async function getSearchResults(searchTerm, params = {}) {
-    const baseString = "/v3/search/" + encodeURIComponent(searchTerm)
-    return makePTVrequest(baseString, params);
-}
-
-async function getDirectionsForRoute(routeId, params = {}) {
-    const baseString = `/v3/directions/route/${routeId}`
-    return makePTVrequest(baseString, params);
-}
-
-async function getDirections(directionId, routeType, params = {}) {
-    let baseString = `/v3/directions/${directionId}`
-    if (routeType) {
-        baseString += `/route_type/${routeType}`
+    async getSearchResults(searchTerm, params = {}) {
+        const baseString = "/v3/search/" + encodeURIComponent(searchTerm)
+        return this.makePTVrequest(baseString, params);
     }
-    return makePTVrequest(baseString, params);
-}
 
-async function getStoppingPatternDetails(runId, routeType, expandList, params = {}) {
-    const baseString = `/v3/pattern/run/${runId}/route_type/${routeType}`
-    const newParams = {}
-    for (const [key, value] in Object.entries(params)) {
-        newParams[key] = value;
+    async getDirectionsForRoute(routeId, params = {}) {
+        const baseString = `/v3/directions/route/${routeId}`
+        return this.makePTVrequest(baseString, params);
     }
-    if (expandList) {
-        newParams["expand"] = expandList;
+
+    async getDirections(directionId, routeType, params = {}) {
+        let baseString = `/v3/directions/${directionId}`
+        if (routeType) {
+            baseString += `/route_type/${routeType}`
+        }
+        return this.makePTVrequest(baseString, params);
     }
-    return makePTVrequest(baseString, params);
 
-}
+    async getStoppingPatternDetails(runId, routeType, expandList, params = {}) {
+        const baseString = `/v3/pattern/run/${runId}/route_type/${routeType}`
+        const newParams = {}
+        for (const [key, value] in Object.entries(params)) {
+            newParams[key] = value;
+        }
+        if (expandList) {
+            newParams["expand"] = expandList;
+        }
+        return this.makePTVrequest(baseString, params);
 
-async function getRoutes(routeId, params = {}) {
-    const baseString = `/v3/routes/${routeId}`
-    return makePTVrequest(baseString, params);
-}
-
-async function getRunsForRoute(routeId, routeType, params = {}) {
-    let baseString = `/v3/stops/route/${routeId}`
-    if (routeType) {
-        baseString += `/route_type/${routeType}`
     }
-    return makePTVrequest(baseString, params);
-}
 
-// async function getRuns(runId, routeType, params = {}) {
-//
-// }
+    async getRoutes(routeId, params = {}) {
+        const baseString = `/v3/routes/${routeId}`
+        return this.makePTVrequest(baseString, params);
+    }
 
-async function getStopInfo(stopId, routeType, params = {}) {
-    const baseString = `/v3/stops/${stopId}/route_type/${routeType}`
-    return makePTVrequest(baseString, params);
-}
+    async getRunsForRoute(routeId, routeType, params = {}) {
+        let baseString = `/v3/stops/route/${routeId}`
+        if (routeType) {
+            baseString += `/route_type/${routeType}`
+        }
+        return this.makePTVrequest(baseString, params);
+    }
 
-async function getStopsForRoute(routeId, routeType, params = {}) {
-    const baseString = `/v3/stops/route/${routeId}/route_type/${routeType}`
-    return makePTVrequest(baseString, params);
-}
+    // async getRuns(runId, routeType, params = {}) {
+    //
+    // }
 
-async function getNearbyStops(latitude, longitude, params = {}) {
-    const baseString = `/v3/stops/location/${latitude},${longitude}`
-    return makePTVrequest(baseString, params);
+    async getStopInfo(stopId, routeType, params = {}) {
+        const baseString = `/v3/stops/${stopId}/route_type/${routeType}`
+        return this.makePTVrequest(baseString, params);
+    }
+
+    async getStopsForRoute(routeId, routeType, params = {}) {
+        const baseString = `/v3/stops/route/${routeId}/route_type/${routeType}`
+        return this.makePTVrequest(baseString, params);
+    }
+
+    async getNearbyStops(latitude, longitude, params = {}) {
+        const baseString = `/v3/stops/location/${latitude},${longitude}`
+        return this.makePTVrequest(baseString, params);
+    }
 }
 
 // ----------------------------------------------
@@ -179,13 +184,16 @@ async function main() {
     return result
 }
 
-const dToS = (d) => (d ? d.getHours() + ":" + ("0" + d.getMinutes()).slice(-2) : null);
+let myPtv = new PTV(local_vars);
+myPtv.getSearchResults("Murrumbeena", {route_types: ['0']}).then(console.log);    
 
-main().then((result) => {
-    console.log(
-        result.map(({name, sch, est}) => (`${name} \t sch ${dToS(sch)} \t est ${dToS(est)}`)).join('\n')
-    );
-});
+// const dToS = (d) => (d ? d.getHours() + ":" + ("0" + d.getMinutes()).slice(-2) : null);
+
+// main().then((result) => {
+//     console.log(
+//         result.map(({ name, sch, est }) => (`${name} \t sch ${dToS(sch)} \t est ${dToS(est)}`)).join('\n')
+//     );
+// });
 
 
 
